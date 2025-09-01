dffec3db55033ead875d12767a9cf27e19fccb5e
jonathan Service <jonathanservice@jonathans-MacBook-Air.local>
2025-09-01 09:54:02 +0100

    Fix name of next.config.ts to next.config.js

---
+++ next.config.js
@@ -0,0 +1,43 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // This prevents ESLint warnings from failing the production build.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cortexcart.com',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.fbcdn.net', // This allows all subdomains of fbcdn.net
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https'
+        hostname: 'upload.wikimedia.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;
