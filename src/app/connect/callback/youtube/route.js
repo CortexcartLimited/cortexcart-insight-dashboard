62e0ab27e72be9fa331b040aa85fe748faa7a92d
jonathan Service <jonathanservice@jonathans-MacBook-Air.local>
2025-09-03 09:27:17 +0100

    Fixes

--- src/app/connect/callback/youtube/route.js
+++ src/app/connect/callback/youtube/route.js
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next-server';
+import { NextResponse } from 'next/server';
 import { db } from '@/lib/db';
 import { encrypt } from '@/lib/crypto';
 import axios from 'axios';
